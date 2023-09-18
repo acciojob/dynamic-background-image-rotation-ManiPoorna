c810e7920b5df5bda972d5d3846a9fc0fc7ecc3e
Mani Poorna <118202451+ManiPoorna@users.noreply.github.com>
2023-09-18 10:42:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+window.addEventListener("resize",changeBackgroundImage);
+changeBackgroundImage();
+function changeBackgroundImage(){
+    console.log("resize")
+	const body = document.body;
+	if(window.innerWidth > window.innerHeight){
+		body.classList.add("landscape");
+		body.classList.remove("portrait");
+	}
+    else{
+        body.classList.remove("landscape");
+		body.classList.add("portrait");
+	}
+}
